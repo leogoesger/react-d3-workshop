10450d41d4f4ad57196d56199e934e8545126984
Leo Q <leoq91@gmail.com>
2019-02-08 10:40:39 -0800

    1-load: load use useEffect

--- src/d3/LinePlot.tsx
+++ src/d3/LinePlot.tsx
@@ -4,5 +4,18 @@ import * as d3 from 'd3';
 import { loadData } from './loader';
 
 export const LinePlot = () => {
-  return <div>hello</div>;
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    setData(loadData());
+  }, []);
+
+  const _getRandom = () => setData(loadData());
+
+  return (
+    <div style={{ height: '600px' }}>
+      {JSON.stringify(data)}
+      <button onClick={_getRandom}>Get Random Data</button>
+    </div>
+  );
 };
