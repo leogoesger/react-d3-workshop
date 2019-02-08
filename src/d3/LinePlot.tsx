6602e6fbb231acd35e888c56f36f01590061beae
Leo Q <leoq91@gmail.com>
2019-02-08 14:02:00 -0800

    add line

--- src/d3/LinePlot.tsx
+++ src/d3/LinePlot.tsx
@@ -1,21 +1,52 @@
 import React, { useState, useEffect } from 'react';
 import * as d3 from 'd3';
 
-import { loadData } from './loader';
+import { loadData, IData } from './loader';
 
 export const LinePlot = () => {
-  const [data, setData] = useState({});
+  const width = 800;
+  const height = 500;
 
-  useEffect(() => {
-    setData(loadData());
-  }, []);
+  const xScaleL = d3.scaleTime().range([0, width]);
+  const yScaleL = d3.scaleLinear().range([height, 0]);
+  const line = d3.line<IData>();
 
-  const _getRandom = () => setData(loadData());
+  const randomDS = loadData();
 
-  return (
-    <div style={{ height: '600px' }}>
-      {JSON.stringify(data)}
-      <button onClick={_getRandom}>Get Random Data</button>
-    </div>
-  );
+  const _getDomains = (data: IData[]) => {
+    const yMax = d3.max(data, d => d.flow) as number;
+    const xExt = d3.extent(data, d => d.date) as [number, number];
+
+    return { xExtend: xExt, yExtend: [0, yMax] };
+  };
+
+  const { xExtend, yExtend } = _getDomains(randomDS);
+  xScaleL.domain(xExtend);
+  yScaleL.domain(yExtend);
+
+  line
+    .x(d => xScaleL(d.date as any))
+    .y(d => yScaleL(d.flow as any))
+    .curve(d3.curveCardinal);
+
+  // useEffect(() => {}, []);
+
+  if (line && randomDS) {
+    return (
+      <div style={{ height: '600px' }}>
+        <svg style={{ height: '600px', width: '1000px' }}>
+          <path
+            d={line(randomDS) as string}
+            strokeLinecap="round"
+            strokeWidth="3"
+            stroke={'red'}
+            fill="none"
+          />
+        </svg>
+        <button>Get Random Data</button>
+      </div>
+    );
+  } else {
+    return null;
+  }
 };
--- src/d3/loader.ts
+++ src/d3/loader.ts
@@ -1,21 +1,12 @@
+export interface IData {
+  date: number;
+  flow: number;
+}
 export const loadData = () => {
-  const dates = [
-    '10/1/1991',
-    '10/2/1991',
-    '10/3/1991',
-    '10/4/1991',
-    '10/5/1991',
-    '10/6/1991',
-    '10/7/1991',
-    '10/8/1991',
-    '10/9/1991',
-    '10/10/1991',
-  ];
-
-  const flows: number[] = [];
-  for (let i = 0; i <= 10; i++) {
-    flows.push(Math.random() * 100);
+  const data = [] as IData[];
+  for (let i = 0; i <= 100; i++) {
+    data.push({ date: i, flow: Math.random() * 100 });
   }
 
-  return { dates, flows };
+  return data;
 };
